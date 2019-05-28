f931b0f2e4390607999cb0680c8cf856817f4921
jtw1984 <watsonjamest@gmail.com>
2019-05-28 21:37:01 +0000

    Add geocoder and google map api to website

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,9 @@
 class Place < ApplicationRecord
   paginates_per 5
   belongs_to :user
-  validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode
+  validates :name, length: {minimum: 3}, presence: true
+  validates :address, presence: true
+  validates :description, length: {minimum: 10, maximum: 200 }, presence: true
 end
