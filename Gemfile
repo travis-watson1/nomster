3fdec5ce982f5cb2c7d0e08a2dbcf4de384738e7
jtw1984 <watsonjamest@gmail.com>
2019-06-26 15:39:53 +0000

    Update image settings

--- Gemfile
+++ Gemfile
@@ -72,6 +72,7 @@ gem 'geocoder'
 gem 'figaro'
 gem 'carrierwave', '~> 1.0'
 gem 'mini_magick'
+gem "fog-aws"
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,11 +1,11 @@
 class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+   storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -22,7 +22,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  process resize_to_fill: [300,300]
   #
   # def scale(width, height)
      # do something
